53fa9ba71858ca3394f90482626d543a6dde6622
Ryan Pessa <ryan@essential-elements.net>
2016-02-10 13:47:37 -0600

    create strmanip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='StrManip',
+      version='1.0',
+      description='clipboard string manipulation in Python',
+      author='Ryan Pessa',
+      author_email='dkived@gmail.com',
+      url='https://github.com/kived/strmanip',
+      py_modules=['strmanip'])
